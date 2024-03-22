550ff5ced357bac0eefaada9cab718cc62338b79
Your Usernamemadeleinekurtz <Your GitHub email addressmaddiekurtz123@gmail.com>
2024-03-22 16:38:22 -0300

    commit!

--- make_phytoplankton_plot.R
+++ make_phytoplankton_plot.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 
 # Load data
-data <- read.csv("phytoplankton_data.csv", header =TRUE)
+data <- read.csv("data/phytoplankton_data.csv", header =TRUE)
 
 # Make plot
 ggplot(data, aes(x=month, y=phytoplankton)) + 
